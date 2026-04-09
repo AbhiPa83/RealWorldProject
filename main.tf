ba842109756a5107b6447f9a98a263d7faf38bc9
AbhiPa83 <abhipa83@outlook.com>
2026-04-09 10:13:32 +0530

    updated CIDR value for VPC and Subnets

--- main.tf
+++ main.tf
@@ -4,15 +4,15 @@ provider "aws" {
 
 module "vpc" {
   source     = "./modules/vpc"
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "192.168.0.0/16"
 }
 
 module "subnets" {
   source = "./modules/subnets"
 
   vpc_id          = module.vpc.vpc_id
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  public_subnets  = ["192.168.1.0/24", "192.168.2.0/24"]
+  private_subnets = ["192.168.3.0/24", "192.168.4.0/24"]
 }
 
 module "sg" {
